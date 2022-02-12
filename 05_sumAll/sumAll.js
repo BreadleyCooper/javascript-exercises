35523f137193ca3af4db87f6d4fd725d26425f9d
Jordan Ulmer <julmer526@gmail.com>
2022-02-12 17:55:49 +0000

    Completed first two tests for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,19 @@
 const sumAll = function(a,b) {
-    let sum = 0
+    // Convert the inputs into an array, sort by size
+    let array = [a,b];
+    array.sort(function(a, b){return a - b});
+    // Create a variable for the sum
+    let sum = 0;
+    // Set a and b as ints(Bigints?) from the sorted array
+    a = array[0];
+    b = array[1];
+    // Loop that starts at the lowest number of the two and adds one to the sum each time until the larger number (inclusive) is reached)
     for (let i=a;i<=b;i++){
         sum += a++;
-    }
+    } 
 return sum    
 };
 
 
-// Do not edit below this line
+// Do not edit below this line  
 module.exports = sumAll;
