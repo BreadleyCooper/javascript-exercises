b0685f77b8924e9a99ae0afab1a2e3a063e106d8
Jordan Ulmer <julmer526@gmail.com>
2022-02-13 13:41:51 +0000

    na

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,25 @@
 const sumAll = function(a,b) {
-    // Convert the inputs into an array, sort by size
+    // Check the input is correct
+    if ((a < 0) || (b < 0)){
+        return ("ERROR");
+    } else if (isNaN (a) || isNaN (b)) {
+        return ("ERROR");
+    } else {
+    // Convert to array and sort by value low to high
     let array = [a,b];
     array.sort(function(a, b){return a - b});
     // Create a variable for the sum
-    let sum = 0;
+    let sum = 0n;
     // Set a and b as ints(Bigints?) from the sorted array
-    a = array[0];
-    b = array[1];
+    a = BigInt(array[0]);
+    b = BigInt(array[1]);
     // Loop that starts at the lowest number of the two and adds one to the sum each time until the larger number (inclusive) is reached)
     for (let i=a;i<=b;i++){
         sum += a++;
     } 
 return sum    
-};
+}};
+
 
 
 // Do not edit below this line  
