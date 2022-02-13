4d42a90dc3619630469913fc39f1538e3f87dd80
Jordan Ulmer <julmer526@gmail.com>
2022-02-13 16:23:46 +0000

    05 Complete!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,24 +1,44 @@
+// const sumAll = function(a,b) {
+//     // Check the input is correct
+//     if ((a < 0) || (b < 0)){
+//         return ("ERROR");
+//     } else if (isNaN (a) || isNaN (b)) {
+//         return ("ERROR");
+//     } else {
+//     // Convert to array and sort by value low to high
+//     let array = [a,b];
+//     array.sort(function(a, b){return a - b});
+//     // Create a variable for the sum
+//     let sum = 0n;
+//     // Set a and b as ints(Bigints?) from the sorted array
+//     a = BigInt(array[0]);
+//     b = BigInt(array[1]);
+//     // Loop that starts at the lowest number of the two and adds one to the sum each time until the larger number (inclusive) is reached)
+//     for (let i=a;i<=b;i++){
+//         sum += a++;
+//     } 
+// return sum    
+// }};
 const sumAll = function(a,b) {
     // Check the input is correct
     if ((a < 0) || (b < 0)){
-        return ("ERROR");
-    } else if (isNaN (a) || isNaN (b)) {
-        return ("ERROR");
-    } else {
+        return ("ERROR")};
+    if (typeof a !== 'number' || typeof b !== "number") {
+        return ("ERROR")};
     // Convert to array and sort by value low to high
     let array = [a,b];
     array.sort(function(a, b){return a - b});
     // Create a variable for the sum
-    let sum = 0n;
+    let sum = 0;
     // Set a and b as ints(Bigints?) from the sorted array
-    a = BigInt(array[0]);
-    b = BigInt(array[1]);
+    a = array[0];
+    b = array[1];
     // Loop that starts at the lowest number of the two and adds one to the sum each time until the larger number (inclusive) is reached)
     for (let i=a;i<=b;i++){
         sum += a++;
     } 
 return sum    
-}};
+};
 
 
 
