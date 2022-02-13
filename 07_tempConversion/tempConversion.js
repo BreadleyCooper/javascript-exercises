3cb036db6d963e0c0b64c54f3c5039dbaf3e4c99
Jordan Ulmer <julmer526@gmail.com>
2022-02-13 17:39:57 +0000

    Completed 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempc = (temp - 32)*0.556 
+  return Math.round(tempc*10)/10 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let tempf = temp *(9/5) + 32
+  return Math.round(tempf*10)/10 ;
 
 };
 
