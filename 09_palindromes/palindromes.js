47ce76ee11145093313254b4fd5e3c86d83cdce1
Jordan Ulmer <julmer526@gmail.com>
2022-03-01 19:34:00 +0000

    completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // remove all punctuation and spaces from initial string
+    let noPunc = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    let finalString = noPunc.toLowerCase().replace(/\s/g,"")
+    // convert to array
+    let forwardArray = Array.from(finalString)
+    // make new array that is reversed
+    let reverseArray = [...forwardArray].reverse();
+    let reverseString = reverseArray.join("")
+    // compare fwd and reverse array
+    if (finalString === reverseString){
+        return true
+    }else return false
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
