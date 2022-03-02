bc683a87c3f874e2098c0c542465924e1244131c
Jordan Ulmer <julmer526@gmail.com>
2022-03-02 18:37:38 +0000

    completed 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    },
+    {
+      title: "Book3",
+      author: "Name3"
+    }
+  ]
+const getTheTitles = function(array) {
+    return books.map(book => book.title)
 };
 
 // Do not edit below this line
